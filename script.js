00e11ded3b159ff1a09940608a63f97150798469
ruth161 <ruthpandi9@gmail.com>
2024-12-11 21:18:16 -0500

    Proyecto 1
    Versión 1

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  $('.one-time').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+  });
+});
